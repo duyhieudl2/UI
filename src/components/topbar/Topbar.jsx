989b0c735f76835a8e929d7c28249cebf9bb2cee
Administrator <Administrator@BRCNTT7>
2022-12-13 17:35:15 +0700

    ok

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import "./topbar.css"
-import { NotificationsNone, Language, Settings } from '@material-ui/icons';
+import {  SettingOutlined } from '@ant-design/icons';
 
 export default function Topbar() {
   return (
@@ -11,15 +11,15 @@ export default function Topbar() {
             </div>
             <div className="topRight">
                 <div className="topbarIconContainer">
-                    <NotificationsNone />
+                    {/* <NotificationsNone /> */}
                     <span className="topIconBadge">2</span>
                 </div>
                 <div className="topbarIconContainer">
-                    <Language />
+                    {/* <Language /> */}
                     <span className="topIconBadge">2</span>
                 </div>
                 <div className="topbarIconContainer">
-                    <Settings />
+                <SettingOutlined />
                 </div>
                 <img src="https://media.gettyimages.com/id/509819877/photo/woman-punting-bamboo-raft-across-lake.jpg?s=612x612&w=gi&k=20&c=3OecxmB_tTVPH9nj1CPKJi562Noo7DqaebBI-QNCgZA=" alt="" className="topAvatar" />
 
--- src/pages/userList/CreateOrEditUser.jsx
+++ src/pages/userList/CreateOrEditUser.jsx
@@ -1,112 +1,142 @@
 import React from 'react';
-import { Button, Row, Col, Form, Input } from 'antd';
+import { Button, Row, Col, Form, Input, Select } from 'antd';
 import * as userServices from '../../api/userServices'
 
 
 export default function CreateUser() {
 
-    const onFinish = (values) => {
-      console.log('Success:', values);
-      const fetchApi = async () => {
-        const result = await userServices.createUser(values);
-        console.log(result);
-        // setUserList(result);
-      }
-      fetchApi();
+  const onFinish = (values) => {
+    console.log('Success:', values);
+    const fetchApi = async () => {
+      const result = await userServices.createUser(values);
+      console.log(result);
+      // setUserList(result);
+    }
+    fetchApi();
 
-    };
-    const onFinishFailed = (errorInfo) => {
-      console.log('Failed:', errorInfo);
-    };
+  };
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
 
   return (
     <Form
-    name="basic"
-    labelCol={{
-      span:  6,
-    }}
-    wrapperCol={{
-      span: 18,
-    }}
-    initialValues={{
-      remember: true,
-    }}
-    onFinish={onFinish}
-    onFinishFailed={onFinishFailed}
-    autoComplete="off"
-  >
-
-    <Row>
-      <Col span={12}>
-      <Form.Item
-      label="Username"
-      name="userName"
-      rules={[
-        {
-          required: true,
-          message: 'Please input your username!',
-        },
-      ]}
-    >
-      <Input />
-    </Form.Item>
-
-    <Form.Item
-      label="Password"
-      name="password"
-      rules={[
-        {
-          required: true,
-          message: 'Please input your password!',
-        },
-      ]}
-    >
-      <Input.Password />
-    </Form.Item>
-      </Col>
-      <Col span={12}>
-      <Form.Item
-      label="Username"
-      name="userName"
-      rules={[
-        {
-          required: true,
-          message: 'Please input your username!',
-        },
-      ]}
-    >
-      <Input />
-    </Form.Item>
-
-    <Form.Item
-      label="Password"
-      name="password"
-      rules={[
-        {
-          required: true,
-          message: 'Please input your password!',
-        },
-      ]}
-    >
-      <Input.Password />
-    </Form.Item>
-      </Col>
-    </Row>
-
-   
-
-    
-    <Form.Item
-      wrapperCol={{
-        offset: 12,
-        span: 12,
+      name="basic"
+      labelCol={{
+        span: 6,
       }}
+      wrapperCol={{
+        span: 18,
+      }}
+      initialValues={{
+        remember: true,
+      }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      autoComplete="off"
     >
-      <Button type="primary" htmlType="submit">
-        Submit
-      </Button>
-    </Form.Item>
-  </Form>
+
+      <Row>
+        <Col span={12}>
+          <Form.Item
+            label="Tên đăng nhập"
+            name="userName"
+            rules={[
+              {
+                required: true,
+                message: 'Tên đăng nhập không được để trống!',
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="Họ và tên"
+            name="fullName"
+            rules={[
+              {
+                required: true,
+                message: 'Họ và tên không được để trống!',
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="Điện thoại"
+            name="phone"
+            rules={[
+              {
+                required: true,
+                message: 'Điện thoại không được để trống!',
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+
+        </Col>
+        <Col span={12}>
+          <Form.Item
+            label="Mật khẩu"
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: 'Mật khẩu không được để trống!',
+              },
+            ]}
+          >
+            <Input.Password />
+          </Form.Item>
+
+          <Form.Item
+            label="Email"
+            name="email"
+            rules={[
+              {
+                required: true,
+                message: 'Email không được để trống!',
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="Chức vụ"
+            name="position"
+            rules={[
+              {
+                required: true,
+                message: 'Chức vụ không được để trống!',
+              },
+            ]}
+          >
+            <Select
+             labelInValue
+             defaultValue={{
+               value: 'lucy',
+               label: 'Lucy (101)',
+             }}
+            />
+          </Form.Item>
+        </Col>
+      </Row>
+
+      <Form.Item
+        wrapperCol={{
+          offset: 12,
+          span: 12,
+        }}
+      >
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
   )
 }
 
